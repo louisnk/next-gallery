65a81808c600fd7fbef554f6f11937a888a9943a
Louis Kinley <louis@lynk-me.com>
2024-11-27 19:13:30 -0500

    Update env name

--- app/api/media/route.ts
+++ app/api/media/route.ts
@@ -48,7 +48,7 @@ export const runtime = "edge"
 //   formData.append("metadata", JSON.stringify(metadata))
 
 //   const response = await fetch(
-//     `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ACCOUNT_ID}/images/v2/direct_upload`,
+//     `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v2/direct_upload`,
 //     {
 //       method: "POST",
 //       headers: {
